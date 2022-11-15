0ee19350e03bd2dc83b3bb09d6dcc5cc1414597a
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2022-11-15 15:25:41 +0100

    generate_pdf.sh

--- generate_pdf.sh
+++ generate_pdf.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-for dir in */;
+for dir in */
 do 
-    proper_dir=${dir%/}
-    m0=$(cat $PWD/$proper_dir/index.html.md5sum)
-    m1=$(md5sum $PWD/$proper_dir/index.html  | cut -d' ' -f1)
-    echo $m1 > $PWD/$proper_dir/index.html.md5sum
-    if [ "$m0" != "$m1" ]; then
-        shower pdf --cwd $PWD/$proper_dir -o $PWD/$proper_dir/index.pdf
+    if [ $dir != "ShowerTemplate/" ] && [ $dir != "Conferencias/" ]
+    then 
+        proper_dir=${dir%/}
+        m0=$(cat $PWD/$proper_dir/index.html.md5sum)
+        m1=$(md5sum $PWD/$proper_dir/index.html  | cut -d' ' -f1)
+        echo $m1 > $PWD/$proper_dir/index.html.md5sum
+        if [ "$m0" != "$m1" ]; then
+            shower pdf --cwd $PWD/$proper_dir -o $PWD/$proper_dir/index.pdf
+        fi
     fi
 done
